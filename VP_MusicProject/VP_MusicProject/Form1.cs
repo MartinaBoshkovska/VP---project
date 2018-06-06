52f91962456f3b0d03f9feaf27817787e245731b
MartinaBoshkovska <39411682+MartinaBoshkovska@users.noreply.github.com>
2018-06-06 19:02:46 +0200

    Add files via upload
    Setting up custom Note class

--- VP_MusicProject/VP_MusicProject/Form1.cs
+++ VP_MusicProject/VP_MusicProject/Form1.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Midi;
 
 namespace VP_MusicProject
 {
@@ -20,6 +22,34 @@ namespace VP_MusicProject
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            //OutputDevice outputDevice = ExampleUtil.ChooseOutputDeviceFromConsole();
+            //if (outputDevice == null)
+            //{
+            //    Console.WriteLine("No output devices, so can't run this example.");
+            //    ExampleUtil.PressAnyKeyToContinue();
+            //    return;
+            //}
+            //outputDevice.Open();
+
+            //Console.WriteLine("Playing an arpeggiated C chord and then bending it down.");
+            //outputDevice.SendControlChange(Channel.Channel1, Midi.Control.SustainPedal, 0);
+            //outputDevice.SendPitchBend(Channel.Channel1, 8192);
+            //// Play C, E, G in half second intervals.
+            //outputDevice.SendNoteOn(Channel.Channel1, Pitch.C4, 80);
+            //Thread.Sleep(500);
+            //outputDevice.SendNoteOn(Channel.Channel1, Pitch.E4, 80);
+            //Thread.Sleep(500);
+            //outputDevice.SendNoteOn(Channel.Channel1, Pitch.G4, 80);
+            //Thread.Sleep(500);
+
+            //// Now apply the sustain pedal.
+            //outputDevice.SendControlChange(Channel.Channel1, Midi.Control.SustainPedal, 127);
+
+            //// Now release the C chord notes, but they should keep ringing because of the sustain
+            //// pedal.
+            //outputDevice.SendNoteOff(Channel.Channel1, Pitch.C4, 80);
+            //outputDevice.SendNoteOff(Channel.Channel1, Pitch.E4, 80);
+            //outputDevice.SendNoteOff(Channel.Channel1, Pitch.G4, 80);
         }
     }
 }
---
+++ VP_MusicProject/VP_MusicProject/MyNote.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Midi;
+
+namespace VP_MusicProject
+{
+    public class MyNote
+    {
+        public static readonly int myVelocity = 80;
+        public static readonly Channel myChannel = Channel.Channel1;
+        public int myPitch { get; set; } // values 60-71
+        public int myDuration { get; set; } //values 1 - 4
+
+        public MyNote(int pitch, int duration)
+        {
+            this.myPitch = pitch;
+            this.myDuration = duration;
+        }
+
+        public NoteOnMessage noteStart(OutputDevice outputDevice, int counter)
+        {
+            return new NoteOnMessage(outputDevice, myChannel, (Pitch)myPitch, myVelocity, counter);
+        }
+
+        public NoteOffMessage noteEnd(OutputDevice outputDevice, int counter)
+        {
+            return new NoteOffMessage(outputDevice, myChannel, (Pitch)myPitch, myVelocity, counter + myDuration);
+        }
+
+    }
+}
